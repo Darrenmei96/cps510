787c0f31f439b0c176029b7547d5fb9707c96699
Darren Mei <darren.mei@ryerson.ca>
2016-09-28 19:46:39 -0400

    Initial commit of the db schema in sql

---
+++ tableinit.sql
@@ -0,0 +1,17 @@
+create table Airline(
+	AirlineName VARCHAR2(26)
+	Website VARCHAR2(100)
+)
+
+create table Airport(
+	AirportCode VARCHAR2(4)
+	Address VARCHAR2 (50)
+	AirportName VARCHAR2(20)	
+)
+
+create table Airplane(
+	SerialNum VARCHAR2(16)
+	Model VARCHAR2(16)
+	Manufacturer VARCHAR2(10)
+)
+
