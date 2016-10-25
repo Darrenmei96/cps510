5fd8dca21cdbce82c4224d950939455f98bb841e
Darren Mei <darren.mei@ryerson.ca>
2016-10-25 15:10:16 -0400

    Added the updated sql files

--- tableinit.sql
+++ tableinit.sql
@@ -1,17 +1,140 @@
-create table Airline(
-	AirlineName VARCHAR2(26)
-	Website VARCHAR2(100)
-)
 
-create table Airport(
-	AirportCode VARCHAR2(4)
-	Address VARCHAR2 (50)
-	AirportName VARCHAR2(20)	
-)
+--Create a table for the passenger entity
+create table Passenger(
+    passportnumber varchar2(10) not null,
+    name varchar2(25) default 'Dee Falt' not null,
+    birthdate date,
+    email varchar2(50) not null,
+    phonenumber number default 14161234567,
+    primary key (passportnumber)
+);
 
+--Create a table for the luggage entity
+create table Luggage(
+    weight number default 23,
+    luggageid number not null,
+    primary key (luggageid)
+);
+
+--Create a table for the carries relationship
+create table carries(
+    luggageid number references Luggage(luggageid) on delete cascade,
+    passportnumber varchar2(10) references passenger(passportnumber),
+    primary key (luggageid, passportnumber)
+);
+
+--Create a table for the plane ticket entity
+create table PlaneTicket(
+    ticketnumber number not null,
+    classtype varchar2(25) default 'Economy',
+    seatnumber varchar2(3),
+    primary key (ticketnumber)
+);
+
+--Create a table for the meal plan entity
+create table Mealplan(
+    mealPlanName varchar2(25),
+    primary key (mealPlanName)
+);
+
+--Create a table for the adds on relationship
+create table addsOn(
+	ticketnumber number references planeticket(ticketnumber) on delete cascade,
+    mealplanname varchar2(25) references mealplan(mealplanname) on delete cascade,
+    primary key (ticketnumber, mealplanname)
+);
+
+--Create a table for the books relationship
+create table books(
+    ticketnumber number references PlaneTicket(ticketnumber) on delete cascade,
+    passportnumber varchar2(10) references passenger(passportnumber) on delete cascade,
+    primary key (ticketnumber, passportnumber)
+);
+
+--Create a table for the flight entity
+create table Flight(
+    flightNumber number not null,
+    departureDateTime timestamp not null,
+    arrivalDateTime timestamp not null,
+    primary key (flightNumber)
+);
+
+--Create a table for the confirms relationship
+create table confirms(
+    flightnumber number references Flight(flightnumber) on delete cascade,
+	ticketnumber number references planeticket(ticketnumber) on delete cascade,
+    primary key(flightnumber, ticketnumber)
+);
+
+--Create a table for the airplane entity
 create table Airplane(
-	SerialNum VARCHAR2(16)
-	Model VARCHAR2(16)
-	Manufacturer VARCHAR2(10)
-)
+    serialNumber number not null,
+    airplaneManufacturer varchar2(50),
+    seatLimit number,
+    airplaneModel varchar2(20),
+    primary key (serialNumber)
+);
+
+--Create a table for the flies with relationship
+create table fliesWith(
+    planeserial number references Airplane(serialnumber) on delete cascade,
+	flightnumber number references Flight(flightnumber) on delete cascade,
+    primary key (planeserial, flightnumber)
+);
+
+--Create a table for the employee entity
+create table Employee(
+    EmployeeID number not null,
+    EmployeeName varchar2(25) not null,
+    primary key (EmployeeID)
+);
+
+--Create a table for the ticket agent entity
+create table TicketAgent(
+    EmployeeID number references Employee(EmployeeID) on delete cascade,
+    AssignedBooth varchar2(10)
+);
+
+--Create a table for the security guard entity
+create table SecurityGuard(
+    EmployeeID number references Employee(EmployeeID) on delete cascade,
+    assignedsector varchar2(8)
+);
+
+--Create a table for the airport entity
+create table Airport(
+    airportCode varchar2(4) not null,
+    address varchar2(50),
+    airportName varchar2(50),
+    primary key (airportCode)
+);
+
+--Create a table for the works at relationship
+create table worksAt(
+    employeeid number references Employee(employeeid) on delete cascade,
+    airportCode varchar(4) references Airport(airportCode) on delete cascade,
+    primary key (employeeid, airportCode)
+);
+
+--Create a table for the destines at relationship
+create table destinesAt(
+	flightnumber number references flight(flightnumber) on delete cascade,
+	airportcode varchar2(4) references airport(airportcode) on delete cascade,
+	primary key (flightnumber, airportcode)
+); 
+
+--Create a table for the originates from relationship
+create table originatesFrom(
+	flightnumber number references flight(flightnumber) on delete cascade,
+	airportcode varchar2(4) references airport(airportcode) on delete cascade,
+	primary key (flightnumber, airportcode)
+);
+
+--Create a table for the controls relationship
+create table controls(
+	flightnumber number references flight(flightnumber) on delete cascade,
+	airportcode varchar2(4) references airport(airportcode) on delete cascade,
+	primary key (flightnumber, airportcode)
+);
+
 
---
+++ tablepop.sql
@@ -0,0 +1,73 @@
+--Populate the SQL tables
+insert into Passenger values('A1234569','SAM SMITH','01-Jan-1995','abc@ryerson.ca',4189959894);
+insert into Passenger values('B1234569','TIM MITH','02-Jan-1995','abc@ryerson.ca',4189959894);
+insert into Passenger values('C1234569','AMY SMITH','03-Jan-1985','abc@ryerson.ca',4189958394);
+insert into Passenger values('D1234569','EDWARD TH','04-Feb-1965','abc@ryerson.ca',4189998394);
+insert into Passenger values('E1234569','BILL LO','15-Mar-2013','gro@ryerson.ca',6473859204);
+
+insert​ ​into​ ​Luggage​ ​values(21.22,1232434); 
+insert​ ​into​ ​Luggage​ ​values(10,123245); 
+insert​ ​into​ ​Luggage​ ​values(22.22,1232436); 
+insert​ ​into​ ​Luggage​ ​values(20.2,1232437); 
+insert​ ​into​ ​Luggage​ ​values(26.22,1232438);
+
+insert​ ​into​ ​carries​ ​values(1232434,'A1234569'); 
+insert into carries values(123245, 'B1234569');
+insert into carries values(1232436, 'C1234569');
+
+insert into PlaneTicket values(1234,'Economy','D12');
+insert into PlaneTicket values(1453,'Economy','A12');
+insert into PlaneTicket values(1334,'Economy','B12');
+insert into PlaneTicket values(1534,'Economy','C12');
+insert into PlaneTicket values(1233, 'First Class', 'A4');
+insert into PlaneTicket values(1232, 'Business', 'H5');
+
+insert into Mealplan values('Vegan');
+insert into Mealplan values('Children');
+insert into Mealplan values('Chicken');
+insert into Mealplan values('Pasta');
+
+insert into addsOn values(1234,'Vegan');
+insert into addsOn values(1453,'Vegan');
+insert into addsOn values(1233, 'Chicken');
+insert into addsOn values(1232, 'Pasta');
+
+insert into books values(1234,'A1234569');
+
+insert into Flight values(1506,timestamp'2016-10-05 21:35:00',timestamp'2016-10-06 10:30:00');
+insert into Flight values(1234,timestamp'2016-10-05 21:35:00',timestamp'2016-10-05 23:30:00');
+insert into Flight values(4321,timestamp'2016-10-06 14:35:00',timestamp'2016-10-06 18:30:00');
+insert into Flight values(1324,timestamp'2016-10-05 21:35:00',timestamp'2016-10-06 11:30:00');
+
+insert into confirms values(1506, 1234);
+
+insert into Airplane values(747,'Boeing Commercial Airplanes',200,'747SR-81');
+insert into Airplane values(777,'Airbus Groups',200,'747SR-81');
+insert into Airplane values(700,'Boeing Commercial Airplanes',200,'747SR-81');
+insert into Airplane values(741,'Airbus Groups',200,'747SR-81');
+insert into Airplane values(742,'Boeing Commercial Airplanes',200,'747SR-81');
+
+insert into fliesWith values(747,1506);
+
+insert into Employee values(31245,'John Smith');
+insert into Employee values(4321,'Tom Smith');
+insert into Employee values(12345,'Jerry Smith');
+
+insert into TicketAgent values(31245,'Booth 6');
+
+insert into SecurityGuard values(4321, 'Gate 5');
+insert into SecurityGuard values(12345, 'Gate 8');
+
+insert into Airport values('YYZ','6301 Silver Dart Dr','Toronto Pearson International Airport');
+insert into Airport values('HND', 'Hanedakuko, Ota, Tokyo 144-0041', 'Tokyo International Airport');
+
+insert into worksAt values(31245, 'YYZ');
+insert into worksAt values(4321, 'HND');
+insert into worksAt values(12345, 'YYZ');
+
+insert into destinesAt values(1506, 'YYZ');
+
+insert into originatesFrom values(1506, 'HND');
+
+insert into controls values(1506, 'YYZ');
+
